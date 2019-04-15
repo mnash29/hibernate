505180bbe70f9b03dad64b7d0bb71404f019f10d
mnash29 <mtntrades10@gmail.com>
2019-04-15 16:26:44 -0700

    Update database object commit

---
+++ src/com/hibernatedemo/UpdateTvShow.java
@@ -0,0 +1,49 @@
+package com.hibernatedemo;
+
+import com.hibernatedemo.entity.TvShow;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.logging.Logger;
+
+public class UpdateTvShow {
+
+    public static void main(String [] args) {
+
+        // create logger
+        Logger logger = Logger.getLogger(TvShow.class.getName());
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(TvShow.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            int tvShowId = 1000;
+            String msg;
+
+            // start the session
+            session.beginTransaction();
+
+            // update database object
+            TvShow show = session.get(TvShow.class, tvShowId);
+            msg = "Get complete: " + show.toString();
+            logger.info(msg);
+            logger.info("Making Update");
+            show.setGenre("Comedy Drama");
+
+            // commit the transaction
+            session.getTransaction().commit();
+            logger.info("Complete");
+
+        } finally {
+            factory.close();
+        }
+    }
+}
