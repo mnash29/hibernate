4856f037a71013c464ed13a4711945d759b6c53f
mnash29 <mtntrades10@gmail.com>
2019-04-18 14:13:46 -0700

    Added two ways to delete from table

--- src/com/hibernate/demo/DeleteTvShow.java
+++ src/com/hibernate/demo/DeleteTvShow.java
@@ -6,6 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DeleteTvShow {
@@ -25,8 +26,27 @@ public class DeleteTvShow {
         Session session = factory.getCurrentSession();
 
         try {
-            //
+            int tvShowId = 1;
+
+            // start transaction
+            session.beginTransaction();
+
+            // retrieve show based on id: primary key
+            logger.log(Level.INFO, "Getting TvShow with id: {0}", tvShowId);
+            TvShow show = session.get(TvShow.class, tvShowId);
+
+            // delete the TvShow
+            logger.log(Level.INFO, "Deleting TvShow: {0}", show);
+            session.delete(show);
+
+            // delete TvShow using createQuery
+            logger.log(Level.INFO, "Deleting TvShow with id: 2");
+            session.createQuery("DELETE FROM TvShow WHERE id=2").executeUpdate();
+
+            // commit transaction
+            session.getTransaction().commit();
         } finally {
+            logger.info("Complete!");
             factory.close();
         }
     }
