9e31e9d07a7b42b00a4c28a747207e69f6728668
mnash29 <mtntrades10@gmail.com>
2019-04-18 16:20:33 -0700

    Created InstructorDetail class

---
+++ src/com/hibernate/demo/entity/InstructorDetail.java
@@ -0,0 +1,63 @@
+package com.hibernate.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="instructor_detail")
+public class InstructorDetail {
+
+    // define the fields and annotate
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="youtube_channel")
+    private String youTubeChannel;
+
+    @Column(name="hobby")
+    private String hobby;
+
+    // create constructors
+    public InstructorDetail(){}
+
+    public InstructorDetail(String youTubeChannel, String hobby) {
+        this.youTubeChannel = youTubeChannel;
+        this.hobby = hobby;
+    }
+
+    // generate getters/setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getYouTubeChannel() {
+        return youTubeChannel;
+    }
+
+    public void setYouTubeChannel(String youTubeChannel) {
+        this.youTubeChannel = youTubeChannel;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    // generate toString method
+    @Override
+    public String toString() {
+        return "InstructorDetail{" +
+                "id=" + id +
+                ", youTubeChannel='" + youTubeChannel + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+}
