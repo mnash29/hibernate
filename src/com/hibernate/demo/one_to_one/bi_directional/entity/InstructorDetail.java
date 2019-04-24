b92003a26765d61d53dccead8df3cc34a08d18fc
mnash29 <mtntrades10@gmail.com>
2019-04-24 16:20:49 -0700

    Created new entity for bi-directional association

---
+++ src/com/hibernate/demo/one_to_one/bi_directional/entity/InstructorDetail.java
@@ -0,0 +1,77 @@
+package com.hibernate.demo.one_to_one.bi_directional.entity;
+
+import com.hibernate.demo.entity.Instructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="instructor_detail")
+public class InstructorDetail {
+
+    // define the fields and annotate
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="youtube_channel")
+    private String youTubeChannel;
+
+    @Column(name="hobby")
+    private String hobby;
+
+    // add new field for instructor with getters and setters
+    @OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL)
+    private Instructor instructor;
+
+    // create constructors
+    public InstructorDetail(){}
+
+    public InstructorDetail(String youTubeChannel, String hobby) {
+        this.youTubeChannel = youTubeChannel;
+        this.hobby = hobby;
+    }
+
+    // generate getters/setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getYouTubeChannel() {
+        return youTubeChannel;
+    }
+
+    public void setYouTubeChannel(String youTubeChannel) {
+        this.youTubeChannel = youTubeChannel;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    // generate toString method
+    @Override
+    public String toString() {
+        return "InstructorDetail{" +
+                "id=" + id +
+                ", youTubeChannel='" + youTubeChannel + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+}
