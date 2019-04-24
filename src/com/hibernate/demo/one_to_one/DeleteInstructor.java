855c0d80b096e20d7b21ddfa54ebe17cb8be7fe6
mnash29 <mtntrades10@gmail.com>
2019-04-24 16:00:52 -0700

    Created delete for Instructor CRUD app

---
+++ src/com/hibernate/demo/one_to_one/DeleteInstructor.java
@@ -0,0 +1,54 @@
+package com.hibernate.demo.one_to_one;
+
+import com.hibernate.demo.entity.Instructor;
+import com.hibernate.demo.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DeleteInstructor {
+    public static void main(String [] args) {
+
+        // create logger
+        Logger logger = Logger.getLogger(Instructor.class.getName());
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get instructor by primary key: id
+            int id = 1;
+            Instructor instructor = session.get(Instructor.class, id);
+            logger.log(Level.INFO, "Found instructor: {0}", instructor);
+
+            // delete instructor
+            if(instructor != null) {
+                logger.log(Level.INFO, "Deleting: {0}", instructor);
+
+                // NOTE: will ALSO delete associated "instructorDetails" object
+                // becuase of CascadeType.ALL
+                session.delete(instructor);
+            }
+
+            // commit transaction
+            session.getTransaction().commit();
+
+        } finally {
+            factory.close();
+        }
+    }
+}
